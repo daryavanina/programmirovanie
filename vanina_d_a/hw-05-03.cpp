5b9b4eccf4d49aa5a51c56242f77445266d96ace
daryavanina <114079693+daryavanina@users.noreply.github.com>
2022-10-11 00:09:52 +0300

    Update hw-05-03.cpp

--- vanina_d_a/hw-05-03.cpp
+++ vanina_d_a/hw-05-03.cpp
@@ -1,16 +1,7 @@
-﻿#define _USE_MATH_DEFINES
-#include <iostream>
+﻿#include <iostream>
+#define _USE_MATH_DEFINES
 #include <cmath>
-#include<iomanip>
-double fact(int N)
-{
-	if (N < 0)
-		return 0;
-	if (N == 0)
-		return 1;
-	else
-		return N * fact(N - 1);
-}
+#include <iomanip>
 int main() {
 	double x = 0;//х=а, x∈[a,b]
 	double b = 1;
@@ -18,27 +9,23 @@ int main() {
 	double eps = 0.0001;
 	double s = 0;//сумма
 	double tek = 0;//член суммы
-	int i_nach = 1;
-	//int x = 0;
+	double y = 0;
 	std::cout << "   x    s(x)  f(x)" << std::endl;
 	for (; x <= b; x += step) {
 		tek = 1;
 		s = 0;
-		for (int i = i_nach; i < 100; i++) {
+		for (int i = 1; i < 100; i++) {
 			if (abs(tek) >= eps) {
 				s += tek;
-				tek = pow(2*x,i)/fact(i);
+				tek *= (2*x) / i;
 			}
 			else {
 				break;
 			}
 		}
 		std::cout << std::fixed << std::setprecision(3);
-		double y = 0;
+		
 		y = pow(M_E, 2 * x);
 		std::cout << x << " " << s << " " << y << std::endl;
-		std::cout << std::endl;
 	}
-	//std::cout << fact(4);
 }
-
