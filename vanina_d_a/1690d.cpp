26e0085f57d5ca6b92e7754ede1f8679eff2979a
daryavanina <114079693+daryavanina@users.noreply.github.com>
2022-10-19 15:47:38 +0300

    Create 1690d.cpp

---
+++ vanina_d_a/1690d.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+int main() {
+	int t = 0;
+	std::cin >> t;
+	while (t--) {
+		int n = 0;
+		std::cin >> n;
+		int k = 0;
+		std::cin >> k;
+		std::string s;
+		std::cin >> s;
+		int ans = n;
+		for (int str = 0; str <= n - k; str++) {
+			int tek = 0;//для подсчета текущ колва белых
+			for (int i = 0; i < k; i++) {
+				if (s[i] == 'W') { 
+					tek += 1;
+				}
+			}
+			ans = std::min(ans, tek);
+		}
+		std::cout << ans;
+	}
+}
