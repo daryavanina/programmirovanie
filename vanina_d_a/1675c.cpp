5f4de07f7f1fe68ad9704078b4c763a80885b29e
daryavanina <114079693+daryavanina@users.noreply.github.com>
2022-10-31 21:10:02 +0300

    Create 1675c.cpp

---
+++ vanina_d_a/1675c.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+int main() {
+	int t;
+	std::cin >> t;
+	while (t--) {
+		std::string s;
+		std::cin >> s;
+		int n = s.size();
+		int l = 0;
+		int r = n - 1;
+		for (int i = 0; i < n; i++) {
+			if (s[i] == '1') {
+				l = i;
+			}
+			else {
+				if (s[i] == '0') {
+					r = i;
+					break;
+				}
+		    }
+		}
+		std::cout << r - l + 1 << std::endl;
+	}
+}
